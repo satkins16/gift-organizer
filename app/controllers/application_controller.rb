fd366500c9027f9f590b1b3677347cf2ca42acd5
Scott Atkins <satkins16@gmail.com>
2017-05-09 01:55:01 +0000

    created env

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "gifts"
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @user = User.find(session[:user_id])
+    end
+  end
+
+end

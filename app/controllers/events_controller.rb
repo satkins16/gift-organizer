af00699c8785d70ead9aa085dbe0c732bc2724ba
Scott Atkins <satkins16@gmail.com>
2017-05-11 14:48:35 +0000

    events view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,14 @@ class EventsController < ApplicationController
 
   get '/events' do
     redirect_if_not_logged_in
+    @user = current_user
     @events = User.events
     erb :'events/events'
+  end
+
+  get '/events/new' do
+    redirect_if_not_logged_in
+    @user = current_user
   end
 
 end
